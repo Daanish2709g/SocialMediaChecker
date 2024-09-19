0cf66ae7af4a5e75bafef0571ac4cb950def0328
Daanish ilahi <daanishsumkesula@gmail.com>
2024-09-19 18:53:00 +0530

    Create SocialMediaTool.java
    this is main class

---
+++ SocialMediaTool.java
@@ -0,0 +1,16 @@
+package com.socialmedia.tool;
+
+import java.util.Scanner;
+
+public class SocialMediaTool {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the company's domain name :");
+        String domain = scanner.nextLine();
+        scanner.close();
+
+        SocialMediaChecker checker = new SocialMediaChecker();
+        checker.checkSocialMedia(domain);
+    }
+}
